f48f7b2d12dae0b786e1399b6d2dc03ba275f470
Huang Steve <lkk200045@gmail.com>
2018-08-11 22:56:35 +0800

    add new file

---
+++ first1.py
@@ -0,0 +1,17 @@
+x=5
+y =0.38
+z=x*y
+name = 'huang'
+
+print(x,y,z,name)
+
+name = input('請輸入名子: ')
+print(name)
+
+hight = int(input('請輸入身高: '))
+print(type(hight))
+
+bmi = hight*hight/90
+
+print(bmi)
+
